3b240979b8083961e837ab310a4755168b381ac7
Jongwon <lastone9182@gmail.com>
2016-04-20 14:08:44 +0900

    jquery added

--- components.php
+++ components.php
@@ -1,16 +1,18 @@
 <?php header("Content-Type: text/html; charset=UTF-8");
 
 class Frame {
-	
+
 	public function headernav(){
 		$h = "
 <!doctype html>\n
 <head> \n
-<meta charset='UTF-8'> \n 
-<title> 오라클47기 </title> \n 
+<meta charset='UTF-8'> \n
+<title> 오라클47기 </title> \n
 <link rel='stylesheet' href='/css/style.css' /> \n
 <link rel='stylesheet' href='/css/animate.min.css' />\n
 <link rel='stylesheet' href='/css/zenburn.css' /> \n
+<script type='text/javascript' src='/js/jquery.min.js'></script> \n
+<script type='text/javascript' src='/js/script.js'></script> \n
 </head> \n
 <body> \n
 <ul class='nav'> \n
@@ -25,10 +27,10 @@ class Frame {
 ";
 		return $h;
 	}
-	
+
 	public function footer(){
 		$f = "</body>\n</html>";
 		return $f;
 	}
-	
+
 }
