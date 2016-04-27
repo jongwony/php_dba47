02b92865c089d1d09b09177616427f8c44bba972
jongwon <lastone9182@gmail.com>
2016-04-27 12:09:24 +0900

    datapump source

--- index.php
+++ index.php
@@ -4,7 +4,11 @@
 	$frame = new Frame;
 	print	$frame->headerNav();
 	
+	print "<h1 class='header'>잠은 죽어서 자라</h1>\n";
+	print "<img src='/image/tak.jpg' style='width:30%; margin: auto;' /><br>\n";
+	//print "<img src='/image/mainword.jpg' style='width:50%;margin: auto;' /> \n";
 	
-
+//	print "<div style='text-align:center;'><a href='http://band.us/#!/band/59205795'>BAND</a></div>\n";
+	print "<a href='http://band.us/#!/band/59205795'><img style='width:10%' src='https://partners.band.us/img/developers/share/bandapp.png' /></a>\n";
 
 	print	$frame->footer();
--- query.php
+++ query.php
@@ -6,68 +6,24 @@ print	$frame->headernav();
 
 print "<h1 class='header'>ER Diagram</h1>\n";
 
+print "<div id='sqlquery'></div>\n";
+
 print "<img src='/image/erd.png' /><br>\n";
 
-print "<pre style='width:80%;margin:auto;font-size:18px;'><code class='hljs sql' data-trim contenteditable> </code></pre>\n"; 
+print "<div style='width:80%;margin:auto'>";
+print "<pre style='font-size:18px;'><code data-trim contenteditable> </code></pre>\n"; 
+print "<button class='button' onclick='querycallback()'>Submit!</button>\n";
+print "</div>";
 
-/*
-$conn = oci_connect('guest', 'guest', 'localhost/orcl', 'AL32UTF8');
-if (!$conn) {
-    $e = oci_error();
-    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-}
+$array = array(1,2,3,4);
+$sql = 'select * from dba47.library';
 
-// Prepare the statement
-$stid = oci_parse($conn, 'SELECT * FROM dba47.dba47');
-if (!$stid) {
-    $e = oci_error($conn);
-    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-}
+?>
 
-// Perform the logic of the query
-$r = oci_execute($stid);
-if (!$r) {
-    $e = oci_error($stid);
-    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-}
+<script>hljs.initHighlightingOnLoad();</script>
+<script>function querycallback() {
+//document.getElementById("sqlquery").innerHTML = "<?php $frame->querytab($sql, $array); ?>";
+}</script>
 
-// Fetch the results of the query
-print "<table>\n";
-
-print "<tr><th>ID</th>
-<th> 이 름 </th>
-<th> 연 령</th>
-<th>전화번호</th>
-<th>이메일</th>
-<th>전 공</th></tr>\n";
-
-while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
-    print "<tr>\n";
-    foreach ($row as $item) {
-        print "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
-    }
-    print "</tr>\n";
-}
-print "</table>\n";
-
-oci_free_statement($stid);
-oci_close($conn);
-*/
-/*
-print "<script>
-onmessage = function(event) {
-	importScripts('/js/highlight.js');
-	var result = self.hljs.highlightAuto(event.data);
-	postMessage(result.value);
-}
-document.addEventListener('click', function() {
-	var code = document.querySelector('code');
-	var worker = new Worker(onmessage);
-	worker.onmessage = function(event) { code.innerHTML = event.data; }
-	worker.postMessage(code.textContent);
-})
-</script>
-";
-*/
-
+<?php
 print	$frame->footer();
