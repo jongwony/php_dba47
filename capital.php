ea3988268dde807fc9a24a58e4dfb7be6e69ca88
EC2 Default User <ec2-user@ip-172-31-3-135.ap-northeast-2.compute.internal>
2016-05-03 00:54:13 +0000

    capital modify

--- capital.php
+++ capital.php
@@ -15,6 +15,12 @@ FROM dba47.late_list l, dba47.dba47 d WHERE l.id=d.id  ORDER BY".'"date" desc';
 
 $array = array('이 름','벌 금','날 짜');
 
+$sql2 = "select capital || '원' from dba47.capital";
+$array2 = array('공 금');
+
+$frame->querytab($sql2, $array2);
+
+print '<br><br>';
 
 $frame->querytab($sql, $array);
 
